00a551a6c1b867e43dcbd38eced0420be099eda0
Angeth-Herjok <angethherjok@gmail.com>
2023-02-02 21:44:22 +0300

    variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    var fullname = "mary"
+    var weightInKg = 50
+    var phonenumber = "0739385902"
+    var age = 20
+    var kenyancitizensip: Boolean = true
+    var kenyancitizenship: Boolean = false
+
+    println(age)
+
+
+
+
+
+
+
+
+}
